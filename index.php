1b081111c07f0a63a8239a07252a9800b42e5568
Etudiant <Etudiant@L35-13>
2016-12-19 18:00:53 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Projet git brice ISFCE</h1>
+        <p>Lundi 19 décembre 2016</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
